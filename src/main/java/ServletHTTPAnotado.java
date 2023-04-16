0a8989c3268ca9da16fbe28f2c9d364278fd452f
Osmar de Oliveira Braz Junior <osmarbraz@hotmail.com>
2023-04-16 09:32:27 -0300

    Atualização arquivos de lote.

--- src/main/java/ServletHTTPAnotado.java
+++ src/main/java/ServletHTTPAnotado.java
@@ -2,6 +2,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,14 +11,15 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author osmar
  */
-public class ServletHTTP extends HttpServlet {
+@WebServlet(urlPatterns = {"/ServletHTTPAnotado"})
+public class ServletHTTPAnotado extends HttpServlet {
 
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         PrintWriter out = response.getWriter();
         out.println("<html><head></head><body>");
-        out.println("<h1>Hello, World Mapeado!</h1>");
+        out.println("<h1>Hello, World Anotado!</h1>");
         out.println("</body></html>");
         out.close();
     }
